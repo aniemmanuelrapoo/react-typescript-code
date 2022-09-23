7ec891e28fc51f2695644a913d516151b1914a4a
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-09-23 09:31:46 +0100

    object

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css';
 import Greet from './components/Greet';
+import Person from './components/Person';
 
 function App() {
+  const personName ={
+    first: 'Emmanuel',
+    last: 'Rapoo'
+  }
   return (
     <div className="App">
       <Greet name='Rapoo' messageCount={20} isLogin={true} />
+      <Person name={personName} />
     </div>
   );
 }
