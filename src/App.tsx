13966ef5fd1003de64e28f11b810b6c7eccb70e3
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-09-24 02:45:51 +0100

    event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
+import Button from './components/Button';
 import Greet from './components/Greet';
 import Heading from './components/Heading';
+import Input from './components/Input';
 import Oscar from './components/Oscar';
 import Person from './components/Person';
 import PersonList from './components/PersonList';
@@ -36,6 +38,12 @@ function App() {
       <Oscar>
         <Heading>Oscar goes to Tansian University</Heading>
       </Oscar>
+      <Button handleClick={(event, id) => {
+        console.log('Clicked', event, id)
+      }} />
+      <Input value='' handleChange={(event) => {
+        console.log(event);
+      }} />
     </div>
   );
 }
